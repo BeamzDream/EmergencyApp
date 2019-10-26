af52375cd7ef6e3476449eeff36965b2172bc5b9
BeamzDream <43103387+BeamzDream@users.noreply.github.com>
2019-10-26 15:00:28 -0400

    Updated UI for three screens

--- app/src/main/java/com/example/emergencyactivatorapplication/Activities/SecurityWords.java
+++ app/src/main/java/com/example/emergencyactivatorapplication/Activities/SecurityWords.java
@@ -1,7 +1,9 @@
-package com.example.emergencyactivatorapplication;
+package com.example.emergencyactivatorapplication.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.emergencyactivatorapplication.R;
 
 public class SecurityWords extends AppCompatActivity {
 
