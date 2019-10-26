7bdce4aaea2ba9f61d525ad602fb06d1f3967858
BeamzDream <43103387+BeamzDream@users.noreply.github.com>
2019-10-26 15:26:41 -0400

    Linked activities

--- app/src/main/java/com/example/emergencyactivatorapplication/Activities/EmergencyContact.java
+++ app/src/main/java/com/example/emergencyactivatorapplication/Activities/EmergencyContact.java
@@ -1,7 +1,10 @@
 package com.example.emergencyactivatorapplication.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.emergencyactivatorapplication.R;
 
@@ -11,5 +14,14 @@ public class EmergencyContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_contact);
+
+        Button btnContinueEmergencyContact = findViewById(R.id.btnContinueEmergencyContact);
+        btnContinueEmergencyContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(EmergencyContact.this, SecurityWords.class);
+                startActivity(i);
+            }
+        });
     }
 }
